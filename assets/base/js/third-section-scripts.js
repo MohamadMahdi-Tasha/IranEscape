c662df4398d630146064270cc0a0236fd510e3d5
Mohamad Mahdi Tasha <imwhdiiii@gmail.com>
2023-02-12 11:49:21 +0330

    added written scripts to file

---
+++ assets/base/js/third-section-scripts.js
@@ -0,0 +1,17 @@
+// Variables
+const thirdSectionPrevBtn = document.querySelector('.third-section__prev-btn');
+const thirdSectionNextBtn = document.querySelector('.third-section__next-btn');
+
+// Initilazing Third Sections Slider
+const thirdSectionSlider = new Swiper('.third-section__slider', {
+    spaceBetween: 25,
+    slidesPerView: 4,
+    slidesPerGroup: 4,
+    autoplay: {
+        delay: 3000,
+    },
+})
+
+// Adding Event Listener On Each Next And Prev Slide That Slides To Next Or Prev Slide
+thirdSectionPrevBtn.addEventListener('click', () => thirdSectionSlider.slidePrev());
+thirdSectionNextBtn.addEventListener('click', () => thirdSectionSlider.slideNext());

1c3552ae9bc77e894dbc0e84c9c2365b61c02102
Mohamad Mahdi Tasha <imwhdiiii@gmail.com>
2023-02-12 11:48:47 +0330

    added comments and cut them

--- assets/base/js/second-section-scripts.js
+++ assets/base/js/second-section-scripts.js
@@ -20,7 +20,7 @@ secondSectionSlider.on('slideChange', () => {
     const activePaginationItem = document.querySelector('.second-section__pagination-item--active');
     const paginationToActivate = document.querySelector(`.second-section__pagination > .second-section__pagination-item:nth-of-type(${secondSectionSlider.activeIndex + 1})`);
 
-    // Adding Class Of 'first-sect-right-slider__pagination-item--active' To 'paginationToActivate' And Removing It From 'activePaginationItem'
+    // Adding Class Of 'second-section__pagination-item--active' To 'paginationToActivate' And Removing It From 'activePaginationItem'
     activePaginationItem.classList.remove('second-section__pagination-item--active');
     paginationToActivate.classList.add('second-section__pagination-item--active');
 })
